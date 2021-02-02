e2b31107bc49cab426ae6743061c43bd8b24bf86
Paul Sinnett <paul.sinnett@gmail.com>
2021-02-02 01:46:15 +0000

    Initial commit

---
+++ Assets/Player.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public void Jump()
+    {
+        Debug.Log("Jump");
+    }
+
+    public void Attack()
+    {
+        Debug.Log("Attack");
+    }
+}
